0eb1ddb294bf8ee86cc5d51b6383b8f186a6b897
Brian Heim <brianlheim@gmail.com>
2017-07-06 00:03:00 -0400

    Add acts_as_csv_module.rb

---
+++ ruby/acts_as_csv_module.rb
@@ -0,0 +1,45 @@
+# acts_as_csv_module.rb
+# 2017-07-05
+
+module ActsAsCsv
+
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def acts_as_csv
+      include InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+
+    def read
+      @csv_contents = []
+      filename = self.class.to_s.downcase + '.txt'
+      file = File.new(filename)
+      @headers = file.gets.chomp.split(', ')
+
+      file.each do |row|
+        @csv_contents << row.chomp.split(', ')
+      end
+    end
+
+    attr_accessor :headers, :csv_contents
+
+    def initialize
+      read
+    end
+
+  end
+end
+
+class RubyCsv # no inheritance! mixin
+  include ActsAsCsv
+  acts_as_csv
+end
+
+m = RubyCsv.new
+puts m.headers.inspect
+puts m.csv_contents.inspect
