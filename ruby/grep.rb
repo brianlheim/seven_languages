2a6bf27f301705a535770aedf75733765acdc652
Brian Heim <brianlheim@gmail.com>
2017-07-09 22:21:08 -0400

    Fully implement grep, add grep method to String

--- ruby/grep.rb
+++ ruby/grep.rb
@@ -4,8 +4,15 @@ class Grep
   def self.grep(filename, regex)
     f = File.open(filename, "r")
     f.each {|line|
-      puts "#{f.lineno}: #{line}"
+      if regex.match(line)
+        puts "#{f.lineno}: #{line}"
+      end
     }
   end
 end
 
+class String
+  def grep(regex)
+    Grep.grep(self, regex)
+  end
+end
