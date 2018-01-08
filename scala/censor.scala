173d361977d53d9389b1986d11f969b4f27ab7af
Brian Heim <brianlheim@gmail.com>
2018-01-07 21:36:33 -0500

    add censor file and curses.txt

---
+++ scala/censor.scala
@@ -0,0 +1,32 @@
+// Censor trait
+// Replaces curse words with non
+// Brian Heim, 2018-01-06
+import scala.io.Source
+
+trait Censor {
+  val replacements: Array[(String, String)] = {
+    Source.fromFile("curses.txt").getLines()
+      .map { _.split(' ') }
+      .map { ss =>
+        ("\\b" + ss(0) + "\\b", ss(1))
+      }
+      .toArray
+  }
+
+  def censor(s: String): String = {
+    replacements.foldLeft(s) {
+      (partial, censation) => partial.replaceAll(censation._1, censation._2)
+    }
+  }
+}
+
+class Person(greeting: String) {
+  def greet() = println(greeting)
+}
+
+class CensoredPerson(greeting: String) extends Person(greeting) with Censor {
+  override def greet() = println(censor(greeting))
+}
+
+val censed = new CensoredPerson("classic!fuck")
+censed.greet()
